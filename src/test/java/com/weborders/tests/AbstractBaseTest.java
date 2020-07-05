dcd221a6cb001cfdd53d4e4b69887660f6f70a38
tugceydgn <tgcaydogan@hotmail.com>
2020-07-05 13:55:30 -0500

    first commit

---
+++ src/test/java/com/weborders/tests/AbstractBaseTest.java
@@ -0,0 +1,20 @@
+package com.weborders.tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+
+public abstract class AbstractBaseTest {
+
+
+    @BeforeMethod
+    public void setup(){
+
+
+    }
+
+    @AfterMethod
+    public void teardown(){
+
+    }
+}
